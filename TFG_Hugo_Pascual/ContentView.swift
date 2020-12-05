2e6bc59165a95e691b87613f55acac5444acdeda
Hugo Pascual <hpascuad@everis.com>
2020-12-05 15:11:26 +0100

    Initial Commit

---
+++ TFG_Hugo_Pascual/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TFG_Hugo_Pascual
+//
+//  Created by Hugo Pascual Adan on 05/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
