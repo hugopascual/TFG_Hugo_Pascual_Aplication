33940b2143bf4912665fcd048649efa1d2d9b766
Hugo Pascual <hugo.pascual.adan@gmail.com>
2021-01-14 09:02:25 +0100

    trying to implement

---
+++ TFG_Hugo_Pascual/Classes/Managers/TabBar/TabBarManager.swift
@@ -0,0 +1,28 @@
+//
+//  TabBarManager.swift
+//  TFG_Hugo_Pascual
+//
+//  Created by Hugo Pascual Adan on 12/01/2021.
+//
+
+import Foundation
+import UIKit
+
+protocol CustomTabBarDelegate: class {
+	func tabBarButtonPressed(item: UITabBarItem)
+}
+
+class CustomTabBar: NSObject {
+	
+	weak var customDelegate: CustomTabBarDelegate?
+	
+	var tabBar: UITabBarController?
+}
+
+extension CustomTabBar: UITabBarControllerDelegate {
+	
+	func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+		print(item.title ?? "pulsado general")
+		self.customDelegate?.tabBarButtonPressed(item: item)
+	}
+}
---
+++ TFG_Hugo_Pascual/Classes/Managers/TabBar/TabBarModel.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarModel.swift
+//  TFG_Hugo_Pascual
+//
+//  Created by Hugo Pascual Adan on 12/01/2021.
+//
+
+import Foundation
