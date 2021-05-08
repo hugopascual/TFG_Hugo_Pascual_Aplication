ad5c8e9747219754ed8d3397f2893005b1dc2606
Hugo Pascual <hugo.pascual.adan@gmail.com>
2021-05-08 20:15:50 +0200

    products list cell created

---
+++ TFG_Hugo_Pascual/Classes/Presentation/ProductsList/Cells/ProductsListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductsListCell.swift
+//  TFG_Hugo_Pascual
+//
+//  Created by Hugo Pascual Adan on 08/05/2021.
+//
+
+import UIKit
+
+class ProductsListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
